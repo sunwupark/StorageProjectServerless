5dbe388d1a4fb1b67301baab9df7b5aa2a66fe44
sunwu <why@gmail.com>
2023-06-04 18:42:36 +0900

    changed

--- firebase.js
+++ firebase.js
@@ -1,4 +1,17 @@
-import firebaseConfig from "./firebaseConfig.js";
 import localAdmin from "firebase-admin";
 export const admin = localAdmin;
+import dotenv from 'dotenv';
+dotenv.config();
+
+const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID
+};
+
 localAdmin.initializeApp(firebaseConfig);
